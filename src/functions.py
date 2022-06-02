b305365a55d9b22c6edfe750daa44e29a83eb26b
eRafaell <rafalo82@interia.pl>
2022-06-02 20:25:13 +0200

    adding more functions

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from shutil import rmtree
 
 from logging.handlers import RotatingFileHandler
 from time import strftime
@@ -82,7 +84,7 @@ def drop_columns(df: DataFrame, columns_to_drop: List) -> DataFrame:
 
 
 def rename_columns(df: DataFrame, mapper: Dict[str, str]) -> DataFrame:
-    ''' 
+    """ 
     Renames column names of a DataFrame
 
     Args:
@@ -90,23 +92,53 @@ def rename_columns(df: DataFrame, mapper: Dict[str, str]) -> DataFrame:
         mapper (Dict): A dict mapping from the old column names to new names
     Returns:
         DataFrame: DataFrame with changed columns name
-    '''
+    """
     for old_column_name, new_column_name in mapper.items():
         logger.info(f"Column renaming from '{old_column_name}' to '{new_column_name}'")
         df = df.withColumnRenamed(old_column_name, new_column_name)
     return df
 
+
 def filter_df(
     df: DataFrame, column_name: DataFrame.columns, value_to_filtered: List[str]
     ) -> DataFrame:
-    ''' 
+    """ 
     Filters DataFrame columns with given values
 
     Args:
         df (DataFrame): Input DataFrame
-        value_to_filtered: 
+        column_name (DataFrame.columns): column in input Dataframe that will be filtered
+        value_to_filtered: List of values to be filtered
     Returns:
         DataFrame: DataFrame with filtered rows
-    '''
+    """
     logger.info(f"Filtered with {value_to_filtered} values in '{column_name}' column")
     return df.where(col(column_name).isin(value_to_filtered))
+
+
+def save_df_to_csv(df: DataFrame, target_path: str = "./client_data/output.csv"): 
+    """ 
+    Saves the dataframe to one csv file
+
+    Args:
+        df (DataFrame): Input DataFrame
+        target_path (str): path where DataFrame as csv will be saved
+    """
+    # writing df as csv file to temporary folder
+    temp = "temp"
+    df.coalesce(1).write.csv(temp, mode="overwrite", header=True)
+    
+    # getting csv filename
+    for file in os.listdir(temp):
+        if file.endswith(".csv"):
+            filename = f"{temp}/{file}"
+
+    # copying csv file to cliend_data folder
+    os.system(f"cp {filename} {target_path}") 
+
+    # deleting the temporary folder with contained files 
+    for file in os.listdir(temp):
+        os.remove(os.path.join(temp, file))
+    os.rmdir(temp)
+
+    logger.info(f"Saved DataFrame file as {filename} to {target_path}")
--- src/main.py
+++ src/main.py
@@ -4,17 +4,18 @@ from pyspark.sql.types import IntegerType
 
 from functions import *
 
+
 def main(dataset1_path: str, dataset2_path: str, filtered_countries):
     pass
 
-spark_session = (SparkSession.builder.master('local').appName('spark_app').getOrCreate())
+spark_session = (SparkSession.builder.appName("spark_app").getOrCreate())
 
 dataset_one_path = "./input_datasets/dataset_one.csv"
 dataset_two_path = "./input_datasets/dataset_two.csv"
 
 
 if __name__ == "__main__":
-    logger.info(f'Starting app')
+    logger.info(f"Starting app")
 
     df1_raw = read_csv(spark_session, dataset_one_path)
     df2_raw = read_csv(spark_session, dataset_two_path)
@@ -22,27 +23,30 @@ if __name__ == "__main__":
     df1 = df1_raw.alias("df1")
     df2 = df2_raw.alias("df2")
 
-    df1 = (df1.withColumn('id', df1.id.cast(IntegerType())))
-    df2 = (df2.withColumn('id', df2.id.cast(IntegerType())))
+    df1 = (df1.withColumn("id", df1.id.cast(IntegerType())))
+    df2 = (df2.withColumn("id", df2.id.cast(IntegerType())))
     
     df1.printSchema()
 
     df = join_dataframes(df1, df2, "id")
     df.printSchema()
 
-    logger.info('dropping unneeded columns')
-    df = drop_columns(df, ['first_name', 'last_name', 'cc_n'])
+    logger.info("dropping unneeded columns")
+    df = drop_columns(df, ["first_name", "last_name", "cc_n"])
     
 
-    column_rename = {'id': 'client_identifier',
-                     'btc_a': 'bitcoin_address',
-                     'cc_t': 'credit_card_type'}
+    column_rename = {"id": "client_identifier",
+                     "btc_a": "bitcoin_address",
+                     "cc_t": "credit_card_type"}
     
 
     df = rename_columns(df, column_rename)
 
     filtered_list = ["United Kingdom", "Netherlands"]
 
-    df = filter_df(df, 'country', filtered_list)
+    df = filter_df(df, "country", filtered_list)
     df.show()
-    df.count()
+    print(df.count())
+
+    save_df_to_csv(df)
+    
